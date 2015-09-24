87b32c80bab82171ee1e201a8387c3418893702c
Jingo88 <jcory.ng@gmail.com>
2015-09-24 14:52:47 -0400

    bleh

--- battle.rb
+++ battle.rb
@@ -8,17 +8,18 @@ class Battle
 
 		@turn = initiative.sort_by{|character,speed| speed}.reverse
 
-		if @turn[0][0] != "player"
-			puts "it's a goblins turn"
+		@turn.each do |character|
+			if character[0] != "player"
+				puts "it's a goblins turn"
+			elsif character[0] == "player"
+				puts "Now it's your turn what do you want to do?"
+			end
 		end
+
+#who's turn is it? if it is the goblins turn they should either attack or flee
+#if it is the players turn you need to allow the player to choose attack or flee
+#if it is the players turn they need to choose which goblin to attack
 		binding.pry
-		# @monstarr = []
-		# @monstarr.push(player)
-		# @length = enemies.length
-		# @length.times do
-		# 	@monstarr.push(enemies.pop)
-		# end
-		# return @monstarr.sort.reverse
 	end
 
 
